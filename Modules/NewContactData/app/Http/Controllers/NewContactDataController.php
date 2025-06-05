897803b3628debf1e970f47b5230f9ff8d923312
Astomo Pancoro Putro <tomo@kemang.sg>
2025-06-05 06:44:13 +0000

    woocom still error

--- Modules/NewContactData/app/Http/Controllers/NewContactDataController.php
+++ Modules/NewContactData/app/Http/Controllers/NewContactDataController.php
@@ -927,7 +927,7 @@ class NewContactDataController extends Controller
                         'version' => 'wc/v3'
                 ]
                 );        
-      $woo_response = $woocommerce->get('/customers?page=2&per_page=10');
+      $woo_response = $woocommerce->get('customers?page=2&per_page=10');
        /*   // $id = Contacts::orderBy('id','desc')->first()->id;
         $last_id = 0;
         foreach($woo_response as $key){
