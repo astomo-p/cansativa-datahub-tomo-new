2376a27e5ad5e30bf4cfde20f58bd3b3e2afd813
Astomo Pancoro Putro <tomo@kemang.sg>
2025-06-12 07:45:59 +0000

    updated wrong namespaces in newcontactdata model contact types

--- Modules/NewContactData/app/Models/ContactTypes.php
+++ Modules/NewContactData/app/Models/ContactTypes.php
@@ -4,7 +4,7 @@ namespace Modules\NewContactData\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use App\Models\Contacts;
+use Modules\NewContactData\Models\Contacts;
 // use Modules\NewContactData\Database\Factories\ContactTypesFactory;
 
 class ContactTypes extends Model
