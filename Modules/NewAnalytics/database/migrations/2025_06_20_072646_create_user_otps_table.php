d020ab8ffc056db77dc01654cf9871baad6c776f
astomo-p <tomo@kemang.sg>
2025-06-20 14:51:39 +0700

    new migration

---
+++ Modules/NewAnalytics/database/migrations/2025_06_20_072646_create_user_otps_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_otps', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->string('otp')->nullable();
+            $table->timestamp('expired')->nullable();
+            $table->bigInteger('created_by')->nullable();
+            $table->bigInteger('updated_by')->nullable();
+            $table->timestamp('created_date')->useCurrent();
+            $table->timestamp('updated_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_otps');
+    }
+};
